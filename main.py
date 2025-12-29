6755f00847672a4cea7169b014a1ea348a0bca00
Puwa1 <thanpisit1707@gmail.com>
2025-12-29 16:47:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -582,7 +582,7 @@ class GPSTrackingApp(ctk.CTk):
         prev_status = None
         
         # [SETTING] Smooth
-        self.interp_steps = 10 
+        self.interp_steps = 1 
 
         for i in range(len(records)):
             p1 = records[i]
@@ -677,13 +677,22 @@ class GPSTrackingApp(ctk.CTk):
             self.key_loop_job = None
             return
 
-        new_val = self.current_frame + self.current_key_direction
+        # [แก้ตรงนี้] คูณด้วย interp_steps เพื่อกระโดดข้ามจุดหลอก ไปหาจุดจริงทันที
+        step_size = self.interp_steps 
+        new_val = self.current_frame + (self.current_key_direction * step_size)
         
+        # ปัดเศษให้ลงล็อก (เผื่อหลุดเฟรม)
+        if step_size > 1:
+            new_val = (round(new_val / step_size) * step_size)
+
         if 0 <= new_val < len(self.animation_points):
             self.slider.set(new_val) 
             self.perform_update(new_val)
             self.update_idletasks() 
-            self.key_loop_job = self.after(60, self.move_loop)
+            
+            # [CONFIG] ปรับความเร็วตอนกดค้าง (เพิ่มเลข = ช้าลง, ลดเลข = เร็วขึ้น)
+            # แนะนำ 100-150ms เพราะมันกระโดดไกลแล้ว เดี๋ยวตาลาย
+            self.key_loop_job = self.after(120, self.move_loop)
         else:
             self.current_key_direction = 0
             self.key_loop_job = None
